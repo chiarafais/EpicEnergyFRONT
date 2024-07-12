6a389d51b371d49f3d1dff563d9bc3097a3fdf85
chiarafais <chiara0915@gmail.com>
2024-07-12 15:33:58 +0200

    fix CSS

--- EpicEnergyFRONT/src/components/Customer.jsx
+++ EpicEnergyFRONT/src/components/Customer.jsx
@@ -76,12 +76,8 @@ const Customer = () => {
         <>
           <Table striped bordered hover>
             <div className="d-flex space-between">
-              <h1 className="text-white">DETTAGLI CLIENTE</h1>
-              <Button
-                variant="primary"
-                onClick={() => navigate("/main")}
-                className="ms-3"
-              >
+              <h1 className="text-white">Dettagli cliente</h1>
+              <Button variant="primary" onClick={() => navigate("/main")} className="ms-3 buttonTornaIndietro">
                 Torna indietro
               </Button>
             </div>
@@ -94,7 +90,7 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Client Type</strong>
+                  <strong className="textColorGray">Client Type</strong>
                 </td>
                 <td>{customer.clientType}</td>
               </tr>
@@ -106,7 +102,7 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Email</strong>
+                  <strong className="textColorGray">Email</strong>
                 </td>
                 <td>{customer.email}</td>
               </tr>
@@ -118,7 +114,7 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Name Contact</strong>
+                  <strong className="textColorGray">Name Contact</strong>
                 </td>
                 <td>{customer.nameContact}</td>
               </tr>
@@ -130,7 +126,7 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Surname Contact</strong>
+                  <strong className="textColorGray">Surname Contact</strong>
                 </td>
                 <td>{customer.surnameContact}</td>
               </tr>
@@ -142,7 +138,7 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Telephone Customer</strong>
+                  <strong className="textColorGray">Telephone Customer</strong>
                 </td>
                 <td>{customer.telCustomer}</td>
               </tr>
@@ -154,7 +150,7 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Insertion Date</strong>
+                  <strong className="textColorGray">Insertion Date</strong>
                 </td>
                 <td>{customer.insertionDate}</td>
               </tr>
@@ -166,41 +162,41 @@ const Customer = () => {
               </tr>
               <tr>
                 <td>
-                  <strong>Logo Agency</strong>
+                  <strong className="textColorGray">Client Type</strong>
                 </td>
-                <td>{customer.logoAgency}</td>
+                <td>{customer.clientType}</td>
               </tr>
               <tr>
                 <td>
-                  <div className="">
-                    <Button variant="danger" onClick={handleShow} className="">
-                      DeleteClient
-                    </Button>
-                  </div>
+                  <div className=""></div>
                 </td>
-                <td></td>
+                <td className="changePositionButton">
+                  <Button variant="danger" onClick={handleShow} className="deleteClientButton">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="white"
+                      className="bi bi-trash3 mx-1"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                    </svg>
+                    Elimina cliente
+                  </Button>
+                </td>
               </tr>
             </tbody>
           </Table>
           <Modal show={show} onHide={handleClose}>
             <Modal.Header>
-              <Modal.Title>
-                Sicuro di voler eliminare questo cliente?
-              </Modal.Title>
+              <Modal.Title>Sicuro di voler eliminare questo cliente?</Modal.Title>
             </Modal.Header>
             <div className="p-2">
-              <Button
-                variant="danger"
-                onClick={deleteCustomer}
-                className="me-2"
-              >
+              <Button variant="danger" onClick={deleteCustomer} className="me-2">
                 Delete
               </Button>
-              <Button
-                variant="primary"
-                onClick={handleClose}
-                className="saveCustomerButtonModal"
-              >
+              <Button variant="primary" onClick={handleClose} className="saveCustomerButtonModal">
                 Close
               </Button>
             </div>
